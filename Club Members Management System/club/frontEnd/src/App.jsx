38dcd2a4f2d2ef3140083ffdc4153fa5cbf752da
mekdes haftu <haftumekdi@gmail.com>
2023-11-22 20:09:48 +0300

    add home page

--- Club Members Management System/club/frontEnd/src/App.jsx
+++ Club Members Management System/club/frontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+/*import React, { useState } from 'react'
 import { useEffect } from 'react'
 
 function App() {
@@ -29,6 +29,31 @@ function App() {
       </tbody>
     </table>
   </div>
+  )
+}
+
+export default App
+
+*/
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
   )
 }
 
